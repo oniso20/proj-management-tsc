62f2b99931103ffd2edf4cf11dca2f2cc73ec8f5
Oniso20 <emem.c.onis@gmail.com>
2023-01-25 13:19:50 +0200

    Add decorator - autobind

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
-console.log('PMApp in TS begins...');
+function autoBind(
+    _: any, 
+    _2: string, 
+    descriptor: PropertyDescriptor) 
+    {
+
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
 
 class ProjectInput {
     templateElement: HTMLTemplateElement;
@@ -26,13 +41,14 @@ class ProjectInput {
         this.configure();
     }
 
+    @autoBind
     private submitHandler(event: Event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
